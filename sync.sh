650d10cde8efbf5ef374dddbabb97d65ee158078
LaxentaInc <gk559850@gmail.com>
2026-06-28 15:58:56 +0530

    feat: add sync script and update gitignore to ignore synchronization artifacts

---
+++ sync.sh
@@ -0,0 +1,7 @@
+SRC="/media/sf_WallpaperEngine-Linux"
+DEST="$HOME/wallpaperengine-local"
+
+while true; do
+  rsync -a --delete --exclude node_modules --exclude target --exclude .git "$SRC/" "$DEST/"
+  sleep 1
+done
